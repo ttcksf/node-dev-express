afdd77c9bc73bccc2c1c4bbd0a34d76878d3a341
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-22 04:52:14 +0900

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", function (req, res) {
+//   console.log(req);
+//   res.send("<h1>top page</h1>");
+// });
+
+app.post("/api/v1/quiz", function (req, res) {
+  const answer = req.body.answer;
+  if (answer == 2) {
+    // res.send("<h1>正解</h1>");
+    res.redirect("/correct.html");
+  } else {
+    res.redirect("/wrong.html");
+    // res.send("<h1>不正解</h1>");
+  }
+});
+
+app.get("/api/v1/users", function (req, res) {
+  res.send({
+    name: "Mike",
+    age: 18,
+  });
+});
+
+app.listen(3000, function () {
+  console.log("I am running");
+});
